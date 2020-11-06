Pre train update: Fixed a lotta stuff added action masking etc works with 60 percent winrate

Logs/plotter.py
@@ -2,19 +2,21 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 log_cnn = open('cnn_log.txt','r')
-log_dnn = open('dnn_log.txt','r')
+log_dnn = open('ddqn_log.txt','r')
 
 epochs = 1200
 cnn=[]
 dnn=[]
 x = []
 
-for i in range(1200):
-    cnn.append(float(log_cnn.readline()))
-    dnn.append(float(log_dnn.readline()))
+for i in range(17000):
+    #cnn.append(float(log_cnn.readline()))
+    line = log_dnn.readline()[:-2]
+    arr = line.split(',')
+    dnn.append(float(arr[0]))
     x.append(i)
 
-cnn = np.asarray(cnn)
+#cnn = np.asarray(cnn)
 dnn = np.asarray(dnn)
 x = np.asarray(x)
 
@@ -26,6 +28,6 @@ def smooth(y, box_pts):
     return y_smooth
 
 l1, = plt.plot(x, smooth(dnn,50), 'r-', lw=1.5)
-l2, = plt.plot(x, smooth(cnn,50), 'g-', lw=1.5)
-plt.legend(['DNN', 'CNN'])
+#l2, = plt.plot(x, smooth(cnn,50), 'g-', lw=1.5)
+#plt.legend(['DNN', 'CNN'])
 plt.show()

game.py
@@ -1,5 +1,8 @@
 import time
 import numpy as np
+from numpy import multiply,zeros,add,random,count_nonzero
+from numpy import int as intnp
+from numba import njit
 
 class MineSweeper():
     def __init__(self,width,height,bomb_no):
@@ -8,8 +11,6 @@ class MineSweeper():
         ### fog contains the things that are visible to the player/ system :        [   0 = not visible,    1 = visible ]
         ### grid_width, grid_height, bomb_no are self explanatory
         ### bomb_locs contains *flattened* locations of the bombs in the grid
-
-        ### Initialize game parameters
         self.grid_width = width
         self.grid_height = height
         self.bomb_no = bomb_no
@@ -18,34 +19,18 @@ class MineSweeper():
         self.reset()
 
     def reset(self):
-
-        ### Initialize the grid
-        self.grid = np.zeros((self.grid_width,self.grid_height),dtype=np.int)
-        self.fog = np.zeros((self.grid_width,self.grid_height),dtype=np.int)
-        self.state = np.zeros((self.grid_width,self.grid_height),dtype=np.int)
-    
-        self.bomb_locs = np.random.choice(range(self.box_count), self.bomb_no, replace=False)
-
-        ### Plants the bombs and creates the solutions
+        self.grid = zeros((self.grid_width,self.grid_height),dtype=intnp)
+        self.fog = zeros((self.grid_width,self.grid_height),dtype=intnp)
+        self.state = zeros((self.grid_width,self.grid_height),dtype=intnp)
+        self.bomb_locs = random.choice(range(self.box_count), self.bomb_no, replace=False)
         self.plant_bombs()
         self.hint_maker()
         self.update_state()
-
         self.uncovered_count = 0
 
     def update_state(self):
-        self.state = np.multiply(self.grid,self.fog)
-        self.state = np.add(self.state,(self.fog-1))
-
-    def print_state(self):
-        self.print_matrix(self.state)
-
-    def print_matrix(self,matrix):
-        for row in matrix:
-            to_print = ""
-            for ele in row:
-                to_print+=str(ele)+"\t"
-            print(to_print)
+        self.state = multiply(self.grid,self.fog)
+        self.state = add(self.state,(self.fog-1))
 
     def plant_bombs(self):
         reordered_bomb_locs = []
@@ -60,57 +45,50 @@ class MineSweeper():
         for r,c in self.bomb_locs:
             for i in range(r-1,r+2):
                 for j in range(c-1,c+2):
-                    if(i>=0 and j>=0 and i<self.grid_height and j<self.grid_width and self.grid[i][j]!=-1):
+                    if(i>-1 and j>-1 and i<self.grid_height and j<self.grid_width and self.grid[i][j]!=-1):
                         self.grid[i][j]+=1
-    
-    def get_state(self):
-        return self.state.flatten()
-
-    def unfog_zeros(self,i,j):
-        queue = []
-        queue.append((i,j))
-        while(len(queue)>0):
-            i,j = queue.pop()
-            for r in range(i-1,i+2):
-                for c in range(j-1,j+2):
-                    if(r>=0 and r<self.grid_height and c>=0 and c<self.grid_width):
-                        if(self.grid[r][c]==0 and self.fog[r][c]==0):
-                            queue.append((r,c))
-                        self.fog[r][c]=1
-                        
 
     def choose(self,i,j):
-
-        ### If selects something already selected/ uncovered
         if(self.fog[i][j]==1):
-            return self.state,False,-0.01
+            return self.state,False,-0.3
 
-        ### If selected tile has a 0 under it
         if(self.grid[i][j]==0):
-            self.unfog_zeros(i,j)
-            self.uncovered_count=  np.count_nonzero(self.fog)
+            unfog_zeros(self.grid,self.fog,i,j)
+            self.uncovered_count=count_nonzero(self.fog)
             self.update_state()
             if(self.uncovered_count==self.box_count-self.bomb_no):
                 return self.state,True,1
             return self.state,False,0.5
 
-        ### If selected tile has something > 0 under it
         elif(self.grid[i][j]>0):
             self.fog[i][j]=1
-            self.uncovered_count=np.count_nonzero
+            self.uncovered_count=count_nonzero(self.fog)
             self.update_state()
             if(self.uncovered_count==self.box_count-self.bomb_no):
                 return self.state,True,1
             return self.state,False,0.5
 
-        ### If selected tile has a bomb under it
         else:
             return self.state,True,-1
+
+@njit
+def unfog_zeros(grid,fog,i,j):
+    h,w = grid.shape
+    queue = []
+    queue.append((i,j))
+    while(len(queue)>0):
+        i,j = queue.pop()
+        for r in range(i-1,i+2):
+            for c in range(j-1,j+2):
+                if(r>=0 and r<h and c>=0 and c<w):
+                    if(grid[r][c]==0 and fog[r][c]==0):
+                        queue.append((r,c))
+                    fog[r][c]=1
 
 def speed_test(iterations):
     start = time.perf_counter()
     for i in range(iterations):
-        game = MineSweeper(10,10,10,False)
+        game = MineSweeper(10,10,10)
         game.choose(0,0)
     end = time.perf_counter()-start
     return end

tester.py
@@ -1,35 +1,39 @@
 import torch
 import numpy as np
-from dqn import DQN,CNNDQN
+import sys
+sys.path.insert(1,"./Models")
+from ddqn import DDQN
 from renderer import Render
 from game import MineSweeper
 import time
 
 class Tester():
-    def __init__(self,model):
-        if(model=="dnn"):
-            self.dqn = DQN(100,256)
-        elif(model=="cnn"):
-            self.dqn = CNNDQN(10)
-
-        self.render_flag = True
-        self.model_type = model
-        self.width = 10
-        self.height = 10
-        self.env = MineSweeper(self.width,self.height,10)
+    def __init__(self,render_flag):
+        self.model = DDQN(36,36)
+        self.render_flag = render_flag
+        self.width = 6
+        self.height = 6
+        self.env = MineSweeper(self.width,self.height,6)
         if(self.render_flag):
             self.renderer = Render(self.env.state)
-        
-
+        self.load_models(34000)
+    
     def get_action(self,state):
-        if(self.model_type=="dnn"):
-            state = state.flatten()
-        action = self.dqn.act(state)
+        state = state.flatten()
+        mask = (1-self.env.fog).flatten()
+        action = self.model.act(state,mask)
         return action
+
+    def load_models(self,number):
+        path = "pre-trained\ddqn_dnn_gamma99.pth"
+        dict = torch.load(path)
+        self.model.load_state_dict(dict['current_state_dict'])
+        self.model.epsilon = 0
     
     def do_step(self,action):
         i = int(action/self.width)
         j = action%self.width
+
         if(self.render_flag):
             self.renderer.state = self.env.state
             self.renderer.draw()
@@ -39,42 +43,46 @@ class Tester():
     
 
 def win_tester():
-    tester = Tester("dnn")
+    tester = Tester(False)
     state = tester.env.state
+    mask = tester.env.fog
     games = 1000
-    wins =0
-        
-    for i in range(games):
+    wins = 0
+    i=0
+    while(i<games):
         action = tester.get_action(state)
         next_state,terminal,reward = tester.do_step(action)
         state = next_state
-        
         if(terminal):
+            i+=1
             tester.env.reset()
             state = tester.env.state
             if(reward==1):
                 wins+=1
     
-    print(wins/games)
+    print(wins*100/games)
 
 
 def slow_tester():
-    tester = Tester("dnn")
+
+    tester = Tester(True)
     state = tester.env.state
     count = 0
     start = time.perf_counter()
+
     while(True):
         count+=1
         action = tester.get_action(state)
         next_state,terminal,reward = tester.do_step(action)
         state = next_state
         print(reward)
-
-        if(reward!=-0.3):
-            time.sleep(1)
-
+        time.sleep(1)
 
         if(terminal):
+            if(reward==1):
+                print("WIN")
+            else:
+                print("LOSS")
             tester.env.reset()
             state = tester.env.state
             break
@@ -82,7 +90,6 @@ def slow_tester():
         
 
 def main():
-    #win_tester()
     slow_tester()
 
 main()